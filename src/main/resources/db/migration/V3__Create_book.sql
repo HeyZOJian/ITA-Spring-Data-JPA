f7da3eb561551766bd921c2d05241ae469977d6c
vito zhuang <vito.z.j.zhuang@oocl.com>
2018-07-26 19:46:22 +0800

    add create book and book detail sql

---
+++ src/main/resources/db/migration/V3__Create_book.sql
@@ -0,0 +1,5 @@
+CREATE TABLE book (
+  id int NOT NULL AUTO_INCREMENT primary key ,
+  name varchar(255) DEFAULT NULL,
+  book_detail_id int DEFAULT NULL,
+)
---
+++ src/main/resources/db/migration/V4__Create_book_detail.sql
@@ -0,0 +1,5 @@
+CREATE TABLE book_detail (
+  id int NOT NULL AUTO_INCREMENT primary key,
+  number_of_pages int DEFAULT NULL,
+  author_name varchar(20) not null ,
+);
