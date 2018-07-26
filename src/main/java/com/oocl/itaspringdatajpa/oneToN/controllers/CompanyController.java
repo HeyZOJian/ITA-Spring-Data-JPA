7f5f729398afd8f3e857b2a0ad64a25169185abf
vito zhuang <vito.z.j.zhuang@oocl.com>
2018-07-26 19:21:48 +0800

    implement add employee into company

--- src/main/java/com/oocl/itaspringdatajpa/oneToN/controllers/CompanyController.java
+++ src/main/java/com/oocl/itaspringdatajpa/oneToN/controllers/CompanyController.java
@@ -2,9 +2,12 @@ package com.oocl.itaspringdatajpa.oneToN.controllers;
 
 import com.oocl.itaspringdatajpa.oneToN.controllers.dto.CompanyDTO;
 import com.oocl.itaspringdatajpa.oneToN.entities.Company;
+import com.oocl.itaspringdatajpa.oneToN.entities.Employee;
 import com.oocl.itaspringdatajpa.oneToN.repositories.CompanyRepository;
 import com.oocl.itaspringdatajpa.oneToN.repositories.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -33,5 +36,14 @@ public class CompanyController{
 		company.getEmployees().stream()
 				.forEach(employee -> employee.setCompany(company));
 		return companyRepository.save(company);
+	}
+
+	@PutMapping(path = "/{id}")
+	public ResponseEntity updateCompany(@PathVariable int id, @RequestBody Employee employee){
+		Company company = companyRepository.findAllById(id);
+		employee.setCompany(company);
+		employeeRepository.save(employee);
+		companyRepository.save(company);
+		return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
 	}
 }
