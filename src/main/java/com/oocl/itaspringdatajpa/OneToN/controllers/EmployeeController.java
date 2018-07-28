a7ed6f819ee187a4ce8c9833ddfa789a29dd8a64
vito zhuang <vito.z.j.zhuang@oocl.com>
2018-07-28 15:24:24 +0800

    controller return ResponseEntity

---
+++ src/main/java/com/oocl/itaspringdatajpa/OneToN/controllers/EmployeeController.java
@@ -0,0 +1,7 @@
+package com.oocl.itaspringdatajpa.OneToN.controllers;
+
+/**
+ * Created by Vito Zhuang on 7/28/2018.
+ */
+public class EmployeeController {
+}
---
+++ src/main/java/com/oocl/itaspringdatajpa/OneToN/service/CompanyService.java
@@ -0,0 +1,7 @@
+package com.oocl.itaspringdatajpa.OneToN.service;
+
+/**
+ * Created by Vito Zhuang on 7/28/2018.
+ */
+public class CompanyService {
+}
